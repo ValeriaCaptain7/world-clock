5fcd9b31ba5c41bf48f167eeedbc8a57a1fd69d0
ValeriaCaptain7 <pantera3072@gmail.com>
2023-01-22 18:54:01 +0100

    Added font families Orbitron and Space Mono

--- javascript/index.js
+++ javascript/index.js
@@ -7,7 +7,7 @@ let newDehliTimeElement = newDehliElement.querySelector(".time");
 let newDehliTime = moment().tz("Asia/Colombo");
 
 newDehliDateElement.innerHTML = newDehliTime.format("dddd, MMM Do YYYY");
-newDehliTimeElement.innerHTML = newDehliTime.format("h:mm:ss [<small>]A[</small>]");    
+newDehliTimeElement.innerHTML = newDehliTime.format("hh:mm:ss [<small>]A[</small>]");    
 }, 1000);
 
 
@@ -21,7 +21,7 @@ let romeTime = moment().tz("Europe/Rome");
 
 romeDateElement.innerHTML = moment().format("dddd, MMMM Do YYYY");
 romeTimeElement.innerHTML = romeTime.format(
-  "h:mm:ss [<small>]A[</small>]");    
+  "hh:mm:ss [<small>]A[</small>]");    
 }, 1000);
 
 setInterval(function() {
@@ -32,5 +32,5 @@ let torontoTimeElement = torontoElement.querySelector(".time");
 let torontoTime = moment().tz("America/Toronto");
 
 torontoDateElement.innerHTML = moment().format("dddd, MMMM Do YYYY");
-torontoTimeElement.innerHTML = torontoTime.format("h:mm:ss [<small>]A[</small>]");    
+torontoTimeElement.innerHTML = torontoTime.format("hh:mm:ss [<small>]A[</small>]");    
 }, 1000);
